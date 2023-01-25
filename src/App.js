eeec833480d4df2712484213387074f42444337b
Manojkumar886 <maddymano434@gmail.com>
2023-01-25 07:40:19 +0530

    Complted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Mainpage } from "./HomePage";
+import { Mainone } from "./HomePage";
 import { TableForm } from "./TableCreation";
 
 function App() {
   return (
     <>
     <BrowserRouter>
-    <Mainpage/>
+    <Mainone/>
     <Routes>
-      <Route path="update" exact element={<TableForm/>}/>
+      <Route path="new" exact element={<TableForm/>}/>
       <Route path="create" exact element={<h2>Welcome to BrowserRouter Module</h2>} />
     </Routes>
     </BrowserRouter>
